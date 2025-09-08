55417503f106be82490476d335dd03c1e044de75
Satvik Patel <satvikpatel8373@gmail.com>
2025-09-08 19:52:51 +0530

    Vercel deploy: add api/index.js and vercel.json

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Vercel Node.js Serverless Function entrypoint.
+// Export the Express app directly; Vercel treats it as a (req, res) handler.
 const { app } = require('../src/server');
 
 module.exports = app;
--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,6 @@
 
 const mongoose = require('mongoose');
 
-let cachedConnection = global.__mongoose_conn__ || null;
-let cachedPromise = global.__mongoose_promise__ || null;
-
 async function connectToDatabase(mongoUri) {
 	if (!mongoUri) {
 		throw new Error('MONGODB_URI is not defined');
@@ -20,21 +17,6 @@ async function connectToDatabase(mongoUri) {
 	return mongoose.connection;
 }
 
-async function ensureDatabaseConnection(mongoUri) {
-	if (cachedConnection && cachedConnection.readyState === 1) {
-		return cachedConnection;
-	}
-	if (!cachedPromise) {
-		cachedPromise = connectToDatabase(mongoUri).then((conn) => {
-			cachedConnection = conn;
-			global.__mongoose_conn__ = cachedConnection;
-			global.__mongoose_promise__ = cachedPromise;
-			return conn;
-		});
-	}
-	return cachedPromise;
-}
-
-module.exports = { connectToDatabase, ensureDatabaseConnection };
+module.exports = { connectToDatabase };
 
 
